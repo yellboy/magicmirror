b831c6b35170ece841c1b4aab01fda52500def4a
Igor-Jovic <jovic.i24@gmail.com>
2016-11-25 15:00:16 +0100

    fix gpio

--- temperature-service/sensor/TemperatureReader.py
+++ temperature-service/sensor/TemperatureReader.py
@@ -2,9 +2,9 @@ import Adafruit_DHT as dht
 
 
 def readTemperature():
-    h,t = dht.read_retry(dht.DHT22, 4);
+    h,t = dht.read_retry(dht.DHT22, 22);
     return t;
 
 def readHumidity():
-    h,t = dht.read_retry(dht.DHT22, 4);
+    h,t = dht.read_retry(dht.DHT22, 22);
     return h;
