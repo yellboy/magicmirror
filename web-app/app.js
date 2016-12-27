d9d2256ccd000a800639c646af9b4253e29723ef
Marko Cajic <marko.cajic@zuhlke.com>
2016-12-27 17:10:13 +0100

    fadein/fadeout for welcome message

--- web-app/app.js
+++ web-app/app.js
@@ -1,7 +1,16 @@
 angular.module('magicmirror', [ 'ngResource' ]).controller('mainController', mainController);
 
-mainController.$inject = [ 'proximityService', "TemperatureService" ];
+mainController.$inject = ['$interval'];
 
-function mainController(proximityService, TemperatureService) {
-    var vm = this;
+function mainController($interval) {
+    var init = function () {
+        $interval(fadeInOut, 16000);
+    };
+
+    function fadeInOut() {
+        $("#welcomeMessage").fadeOut(4000);
+        $("#welcomeMessage").fadeIn(4000);
+    }
+
+    init();
 }
