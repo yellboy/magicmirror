41ef30720ff0120ef6e93687ac680ea33474ae5b
Aca <coa777@gmail.com>
2017-03-29 22:39:09 +0200

    Removed unneded files

--- startup/magicmirror.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-# webapi.sh
-# run temperature-sensor service
-cd /
-cd home/pi/magicmirror/temperature-service
-sudo python webapi.py
-cd /
--- startup/start.py
+++
@@ -1,5 +0,0 @@
-import subprocess;
-
-subprocess.call('python -m SimpleHTTPServer 8889');
-
-subprocess.call('chromium-browser -kiosk http://localhost:8889/web-app');
